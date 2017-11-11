c2cb49f432ee54e1c6a9b0b37bc82e4e5178c14b
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-11-11 11:23:38 -0200

    Initial commit

---
+++ test/functional/perceptron.test.js
@@ -0,0 +1,14 @@
+import test from 'ava'
+import Perceptron from './../../src/index.js'
+
+test('Make a multilayer perceptron', t => {
+  const layers = [
+    { neurons: 2 },
+    { neurons: 2 },
+    { neurons: 1 },
+  ]
+
+  const perceptron = Perceptron(layers)
+
+  t.is(typeof perceptron, 'function')
+})
